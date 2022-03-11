c72df5cab12f8b070bab486932353a4f3c6cdc2b
Ilya <32096531+Feincraft@users.noreply.github.com>
2022-03-11 17:26:55 +0100

    Add files via upload

---
+++ V1/TaskbarTidal/MainFrame.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace TaskbarTidal
+{
+    partial class MainFrame
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainFrame));
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.lblState = new System.Windows.Forms.Label();
+            this.colorMainForm = new System.Windows.Forms.ColorDialog();
+            this.SuspendLayout();
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoEllipsis = true;
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblOutput.ForeColor = System.Drawing.Color.White;
+            this.lblOutput.Location = new System.Drawing.Point(33, 1);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Padding = new System.Windows.Forms.Padding(0, 6, 0, 0);
+            this.lblOutput.Size = new System.Drawing.Size(103, 27);
+            this.lblOutput.TabIndex = 0;
+            this.lblOutput.Text = "Feincraft Tidy";
+            this.lblOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblOutput.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainFrame_MouseDown);
+            this.lblOutput.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainFrame_MouseMove);
+            // 
+            // lblState
+            // 
+            this.lblState.Font = new System.Drawing.Font("Webdings", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblState.ForeColor = System.Drawing.Color.White;
+            this.lblState.Location = new System.Drawing.Point(0, 1);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(36, 35);
+            this.lblState.TabIndex = 0;
+            this.lblState.Text = "<";
+            this.lblState.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // colorMainForm
+            // 
+            this.colorMainForm.AnyColor = true;
+            this.colorMainForm.FullOpen = true;
+            this.colorMainForm.SolidColorOnly = true;
+            // 
+            // MainFrame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(575, 35);
+            this.Controls.Add(this.lblState);
+            this.Controls.Add(this.lblOutput);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainFrame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tidy";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainFrame_FormClosing);
+            this.Shown += new System.EventHandler(this.MainFrame_Shown);
+            this.DoubleClick += new System.EventHandler(this.MainFrame_DoubleClick);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainFrame_KeyDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainFrame_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainFrame_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Label lblState;
+        private System.Windows.Forms.ColorDialog colorMainForm;
+    }
+}
+
---
+++ V1/TaskbarTidal/MainFrame.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tidy;
+using Tidy.Properties;
+using Windows.Media.Control;
+
+
+namespace TaskbarTidal
+{
+    public partial class MainFrame : Form
+    {
+        System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+        Point lastPoint;
+
+        public MainFrame()
+        {
+            InitializeComponent();
+        }
+
+        private void IfInvoke(Action actionToInvoke)
+        {
+            if (InvokeRequired) _ = Invoke(actionToInvoke);
+            else actionToInvoke();
+        }
+
+        private void MainFrame_Shown(object sender, EventArgs e)
+        {
+            this.BringToFront();
+            timer.Tick += Timer_Tick;
+            timer.Interval = 1000;
+            timer.Enabled = true;
+
+            this.BackColor = Settings.Default.BackColor;
+            this.Left = Settings.Default.LocLeft;
+            this.Top = Settings.Default.LocTop;
+            
+            UpdateMediaData();
+        }
+
+        private async void UpdateMediaData()
+        {
+            var mediaSession = await GlobalSystemMediaTransportControlsSessionManager.RequestAsync();
+            if (mediaSession.GetCurrentSession() is null)
+            {
+                IfInvoke(new Action(() => { 
+                    lblState.Text = Settings.Default.StopIcon;
+                    lblOutput.Text = Messages.NothingPlaying;
+                }));
+                return;
+            }
+            var mediaProperties = await mediaSession.GetCurrentSession().TryGetMediaPropertiesAsync();
+            var playbackState = mediaSession.GetCurrentSession().GetPlaybackInfo().PlaybackStatus;
+
+            IfInvoke(new Action(() => { lblOutput.Text = mediaProperties.Artist + " - " + mediaProperties.Title; }));
+
+            switch (playbackState)
+            {
+                case GlobalSystemMediaTransportControlsSessionPlaybackStatus.Playing:
+                    IfInvoke(new Action(() => { lblState.Text = Settings.Default.PlayIcon; })); break;
+                case GlobalSystemMediaTransportControlsSessionPlaybackStatus.Paused:
+                    IfInvoke(new Action(() => { lblState.Text = Settings.Default.PauseIcon; })); break;
+                default:
+                    IfInvoke(new Action(() => { lblState.Text = Settings.Default.StopIcon; })); break;
+            }            
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        { 
+            this.TopMost = true;
+            UpdateMediaData();
+        }
+
+        private void MainFrame_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Modifiers == Keys.Control)
+            {
+                if (e.KeyCode == Keys.Right) this.Width += 1;
+                if (e.KeyCode == Keys.Left) this.Width -= 1;
+                return;
+            };
+
+            Int32 mov = 10;
+            if (e.Modifiers == Keys.Shift) mov = 1;
+            if (e.KeyCode == Keys.Right) this.Left += mov;
+            if (e.KeyCode == Keys.Left) this.Left -= mov;
+            if (e.KeyCode == Keys.Up) this.Top -= mov;
+            if (e.KeyCode == Keys.Down) this.Top += mov;
+            if (e.KeyCode == Keys.Escape) Application.Exit();
+
+            Settings.Default.LocLeft = this.Left;
+            Settings.Default.LocTop = this.Top;
+        }
+
+        private void MainFrame_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Settings.Default.Save();
+        }
+
+        private void MainFrame_DoubleClick(object sender, EventArgs e)
+        {
+            colorMainForm.Color = this.BackColor;
+            colorMainForm.ShowDialog();
+            this.BackColor = colorMainForm.Color;
+            Settings.Default.BackColor = this.BackColor;
+            Settings.Default.Save();
+        }
+
+        private void MainFrame_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+
+            Settings.Default.LocLeft = this.Left;
+            Settings.Default.LocTop = this.Top;
+        }
+
+        private void MainFrame_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+    }
+}
